e0a32516c9a461b34dab5b19158cb6b2fee2c4db
GabrielDeFaria <gabrieldelfaria@hotmail.com>
2023-05-03 16:19:57 -0300

    style modern add

--- script.js
+++ script.js
@@ -1,98 +1,34 @@
 function pegarTamanho() {
-    tamanho = parseInt(document.getElementById("input").value);
-    document.getElementById("input").value = "";
-  
-    digitos = "0123456789";
-    letras_min = "aãbcçdefghijklmnopqrstuvwxyz";
-    letras_mai = "AÃBCÇDEFGHIJKLMNOPQRSTUVWXYZ";
-    caracteres_esp = "!#$%&*+,-./:;<=>?@^_`|~";
-  
-    valores = digitos + letras_min + letras_mai + caracteres_esp;
-  
-    if (tamanho >= 1) {
-      if (tamanho <= 100) {
-        var senha = formarSenha();
-        exibirNaTela(senha);
-      } else {
-        document.getElementById("resultado").innerHTML =
-          "Senha muito grande (Digite números menores que 100)";
-      }
-    } else {
-      document.getElementById("resultado").innerHTML =
-        "Senha nula ou negativa (Digite números maiores que 0)";
-    }
-  }
-  
-  function formarSenha() {
-    var senha = "";
-  
+  var tamanho = parseInt(document.getElementById("tamanho").value);
+  var digitos = "0123456789";
+  var letras_min = "abcdefghijklmnopqrstuvwxyz";
+  var letras_mai = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var caracteres_esp = "!@#$%^&*()_-+={[}]|\\:;<,>.?/";
+
+  var valores = digitos + letras_min + letras_mai + caracteres_esp;
+  var senha = "";
+
+  if (tamanho >= 1 && tamanho <= 100) {
     for (var i = 0; i < tamanho; i++) {
-      var numeroSorteado = Math.floor(Math.random() * 89);
-      var valor = valores[numeroSorteado];
-  
-      if (valor == ";") {
-        if (senha.indexOf("&") >= 0) {
-          while (numeroSorteado == 78) {
-            numeroSorteado = Math.floor(Math.random() * 89);
-          }
-          valor = valores[numeroSorteado];
-          console.log("teve & seguido de ; e foi trocado");
-        }
-      }
-  
-      senha += valor;
+      var numeroSorteado = Math.floor(Math.random() * valores.length);
+      senha += valores.charAt(numeroSorteado);
     }
-    senha = verificador(senha);
-    return senha;
+    document.getElementById("resultado").innerHTML = " " + senha;
+  } else {
+    document.getElementById("resultado").innerHTML = "Tamanho inválido!";
   }
-  
-  function exibirNaTela(senha) {
-    for (var k = 0; k < senha.length; k++) {
-      if (senha[k] == "<") {
-        senha = senha.replace("<", "&lt;");
-      }
-    }
-    document.getElementById("resultado").innerHTML = "Senha: " + senha;
+}
+
+var tamanhoInput = document.getElementById("tamanho");
+var tamanhoOutput = document.getElementById("tamanho-atual");
+
+tamanhoInput.addEventListener("input", function () {
+  tamanhoOutput.textContent = tamanhoInput.value;
+});
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    var btn = document.querySelector("#btn");
+    btn.click();
   }
-  function verificador(senha) { 
-    var verDigito = 0;
-    var verLetraMin = 0;
-    var verLetraMai = 0;
-    var verChar = 0;
-    if (tamanho < 50) {
-      var numNec = Math.floor(0.25 * tamanho);
-    } else {
-      var numNec = 12;
-    }
-    for (var i = 0; i < senha.length; i++) {
-      if (digitos.indexOf(senha[i]) >= 0) {
-        verDigito += 1;
-      } else if (letras_min.indexOf(senha[i]) >= 0) {
-        verLetraMin += 1;
-      } else if (letras_mai.indexOf(senha[i]) >= 0) {
-        verLetraMai += 1;
-      } else if (caracteres_esp.indexOf(senha[i]) >= 0) {
-        verChar += 1;
-      }
-    }
-  
-    if (
-      verDigito < numNec ||
-      verLetraMin < numNec ||
-      verLetraMai < numNec ||
-      verChar < numNec
-    ) {
-      console.log("Senha fraca trocada: " + senha);
-      senha = formarSenha();
-    }
-    return senha;
-  }
-  
-  document.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      var btn = document.querySelector("#btn");
-  
-      btn.click();
-    }
-  });
-  
+});
